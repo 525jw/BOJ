1cad5186d8df59cdf85ae31a27023b57b18bb040
안재원 <101179346+525jw@users.noreply.github.com>
2024-05-21 11:01:39 +0900

    20240521 1655 AC

--- 2024_05/1655/1655.cpp
+++ 2024_05/1655/1655.cpp
@@ -1,49 +1,37 @@
 #include <iostream>
-#include <stdlib.h>
+#include <queue>
+#include <vector>
+#define undeclared 100001
 using namespace std;
-typedef struct _node{
-    struct _node* llink;
-    struct _node* rlink;
-    int lnum;
-    int rnum;
-    int key;
-}node;
-int N;
-node* createnode(int newkey){
-    node* newnode=(node*)malloc(sizeof(node));
-    newnode->key=newkey;
-    newnode->lnum=0;
-    newnode->rnum=0;
-    newnode->llink=NULL;
-    newnode->rlink=NULL;
-    return newnode;
-}
-node* insertnode(node* root,int newkey){
-    if(root){
-        if(root->lnum < root->rnum){
-            if(root->key>newkey)
-            root->lnum++;
-            root->llink=insertnode(root->llink,newkey);
-        }
-        else if(root->lnum >= root->rnum){
-            root->rnum++;
-            root->rlink=insertnode(root->rlink,newkey);
-        }
-    }
-    else
-        root=createnode(newkey);
-    return root;
-}
 int main(){
-    cin>>N;
-    node* priority_queue=NULL;
+    int N; cin>>N;
+    priority_queue<int,vector<int>,less<int>> pql;
+    priority_queue<int,vector<int>,greater<int>> pqr;
+    int root=undeclared;
     while(N--){
-        int inp;
-        cin>>inp;
-        priority_queue=insertnode(priority_queue,inp);
-        cout<<'#';
-        cout<<priority_queue->lnum<<' '<<priority_queue->rnum<<' ';
-        cout<<priority_queue->key<<endl;
+        int inp; scanf("%d",&inp);
+        if(root==undeclared)
+            root=inp;
+        else{
+            if(inp<root)
+                pql.push(inp);
+            else
+                pqr.push(inp);
+            
+            if(pql.size()>pqr.size()){
+                pqr.push(root);
+                root=pql.top();
+                pql.pop();
+            }
+            else if(pql.size()+1<pqr.size()){
+                pql.push(root);
+                root=pqr.top();
+                pqr.pop();
+            }
+        }
+        // if(!pql.empty()) cout<<"l:"<<pql.top()<<' ';
+        // if(!pqr.empty()) cout<<"r:"<<pqr.top()<<' ';
+        printf("%d\n",root);
     }
     return 0;
 }
